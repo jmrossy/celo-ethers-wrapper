d38601317eee84d853eb369b9165100bad4bdb54
J M Rossy <jm.rossy@gmail.com>
2020-12-06 11:10:44 -0500

    Fix incompatibility with Celo block format Upgrade deps to latest

--- src/lib/CeloProvider.ts
+++ src/lib/CeloProvider.ts
@@ -2,6 +2,20 @@ import { BigNumber, providers, utils } from "ethers";
 import { parseCeloTransaction } from "./transactions";
 
 export class CeloProvider extends providers.JsonRpcProvider {
+  constructor(
+    url?: utils.ConnectionInfo | string,
+    network?: providers.Networkish
+  ) {
+    super(url, network);
+
+    // Override certain block formatting properties that don't exist on Celo blocks
+    // Reaches into https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/formatter.ts
+    const blockFormat = this.formatter.formats.block;
+    blockFormat.gasLimit = () => BigNumber.from(0);
+    blockFormat.nonce = () => "";
+    blockFormat.difficulty = () => 0;
+  }
+
   /**
    * Override to parse transaction correctly
    * https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/base-provider.ts
