449624a8b54cc4c3129fa6fbb6880fab6ecb5e78
anarkrypto <anarkrypto@gmail.com>
2023-02-22 06:45:51 -0300

    Remove env network check - getBaseUrl can handle it

--- test/getHistory.ts
+++ test/getHistory.ts
@@ -4,7 +4,6 @@ async function main() {
     const account = process.env.ACCOUNT;
     if (!account) throw new Error("No ACCOUNT provided in env");
     const network = process.env.NETWORK?.toLocaleLowerCase() || "celo";
-    if (network !== "celo" && network !== "alfajores" && network !== "baklava") throw new Error("Invalid NETWORK provided in env. Use celo, alfajores, or baklava");
     console.info("Using CeloscanProvider with", network.toUpperCase(), "network");
     const provider = new CeloscanProvider(network);
     const history = await provider.getHistory(account);
