3227dc03f07c5e3f0bba6fb48b7312374dc4990e
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-04-16 13:28:40 +0100

    chore(test): adds dotenv and configures file path
    Because the environment variable is located at `tests/.env.test.local` dotenv has to be configured to look for the correct file.
    
    By default it looks for `.env` in the root of the directory.

--- tests/common.ts
+++ tests/common.ts
@@ -1,6 +1,10 @@
 import CeloProvider from "../src/lib/CeloProvider";
 import CeloWallet from "../src/lib/CeloWallet";
 import { ALFAJORES_FORNO, CELO_DERIVATION_PATH } from "./consts";
+import dotenv from 'dotenv';
+
+// Configure dotenv to load .env.test.local
+dotenv.config({ path: 'tests/.env.test.local' });
 
 export function getSigner() {
   const provider = new CeloProvider(ALFAJORES_FORNO);
