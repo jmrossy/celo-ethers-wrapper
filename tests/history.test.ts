69c7b83db27d8deb6d6661990e77f8e30292be1e
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-04-16 18:51:00 +0100

    test(CeloscanProvider): adds TODO note

--- tests/history.test.ts
+++ tests/history.test.ts
@@ -4,7 +4,8 @@ import { getAccount, getSigner } from "./common";
 
 /**
  * TODO(Arthur): Ensure this test runs sequentially after a transfer or transaction has been 
- * triggered in a test suite run. 
+ * triggered in a test suite run or the test runs on an address that is known to have transactions.
+ * 
  * Otherwise a brand new wallet with no transaction history will fail this test.
  * 
  * This test currently fails with my newly initialised and fauceted account.
@@ -19,6 +20,21 @@ test("can fetch an account's history", async () => {
   const network = process.env.NETWORK?.toLocaleLowerCase() || "celo";
   console.log(`Getting history for account ${account} on network ${network}`);
   const provider = new CeloscanProvider(network);
+  /**
+   * TODO(Arthur): `getHistory` makes a request to the `txlist` 
+   * API endpoint. 
+   * 
+   * The call returns 
+   *
+   * > the list of transactions performed by an address, with optional pagination. 
+   * 
+   * Endpoint is called "Get a list of 'Normal' Transactions By Address"
+   * 
+   * Source: https://docs.celoscan.io/api-endpoints/accounts#get-a-list-of-normal-transactions-by-address
+   * 
+   * That means incoming ERC20 transfers, say from a faucet, are not included here.
+   * Only "normal" transactions made by the address are included.
+   */
   const history = await provider.getHistory(account!);
 
   expect(history.length).toBeGreaterThan(0);
