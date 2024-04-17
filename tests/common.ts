0cde16dd4650fd4d3215c6d18720cd0c36784bb1
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-04-17 11:14:39 +0100

    nit(test): renaming Forno variable for ease of reference

--- tests/common.ts
+++ tests/common.ts
@@ -1,13 +1,13 @@
 import CeloProvider from "../src/lib/CeloProvider";
 import CeloWallet from "../src/lib/CeloWallet";
-import { ALFAJORES_FORNO, CELO_DERIVATION_PATH } from "./consts";
+import { FORNO_ALFAJORES_URL, CELO_DERIVATION_PATH } from "./consts";
 import dotenv from 'dotenv';
 
-// Configure dotenv to load .env.test.local
+// Configure dotenv to load test environment variables from `.env.test.local`
 dotenv.config({ path: 'tests/.env.test.local' });
 
 export function getSigner() {
-  const provider = new CeloProvider(ALFAJORES_FORNO);
+  const provider = new CeloProvider(FORNO_ALFAJORES_URL);
   const mnemonic = process.env.MNEMONIC;
   if (!mnemonic) throw new Error("No MNEMONIC provided in env");
   const wallet = CeloWallet.fromMnemonic(
