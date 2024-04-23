8c3891d2586c0c1330b458eba862bfcb523378a6
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-04-23 13:58:07 +0100

    chore: linting

--- src/lib/CeloProvider.ts
+++ src/lib/CeloProvider.ts
@@ -1,90 +1,90 @@
 import {
-    FeeData,
-    JsonRpcProvider,
-    PerformActionRequest,
-    TransactionResponse,
-    TransactionResponseParams,
-    getBigInt,
-    resolveProperties,
+  FeeData,
+  JsonRpcProvider,
+  PerformActionRequest,
+  TransactionResponse,
+  TransactionResponseParams,
+  getBigInt,
+  resolveProperties,
 } from "ethers";
 import { CeloTransactionRequest, parseCeloTransaction } from "./transactions";
 
 export default class CeloProvider extends JsonRpcProvider {
-    async _perform(req: PerformActionRequest): Promise<any> {
-        // Legacy networks do not like the type field being passed along (which
-        // is fair), so we delete type if it is 0 and a non-EIP-1559 network
-        if (req.method === "call" || req.method === "estimateGas") {
-            let tx = req.transaction;
-            if (tx && tx.type != null && getBigInt(tx.type)) {
-                // If there are no EIP-1559 properties, it might be non-EIP-1559
-                if (tx.maxFeePerGas == null && tx.maxPriorityFeePerGas == null) {
-                    const feeData = await this.getFeeData();
-                    if (feeData.maxFeePerGas == null && feeData.maxPriorityFeePerGas == null) {
-                        // Network doesn't know about EIP-1559 (and hence type)
-                        req = Object.assign({}, req, {
-                            transaction: Object.assign({}, tx, { type: undefined }),
-                        });
-                    }
-                }
-            }
-        }
-
-        const request = this.getRpcRequest(req);
-
-        if (request != null) {
-            return await this.send(request.method, request.args);
-        }
-
-        return super._perform(req);
-    }
-
-    /**
-     * Override to handle alternative gas currencies
-     * prepareRequest in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/json-rpc-provider.ts
-     */
-    getRpcRequest(req: PerformActionRequest): null | { method: string; args: Array<any> } {
-        if (req.method === "getGasPrice") {
-            // @ts-expect-error
-            const param = req.feeCurrencyAddress
-                ? // @ts-expect-error
-                  [req.feeCurrencyAddress]
-                : [];
-            return { method: "eth_gasPrice", args: param };
-        }
-
-        if (req.method === "estimateGas") {
-            const extraneous_keys = [
-                ["from", (x: string) => x],
-                ["feeCurrency", (x: string) => x],
-            ] as const;
-
-            const tx = {
-                ...this.getRpcTransaction(req.transaction),
-            };
-            extraneous_keys.forEach(([key, fn]) => {
-                // @ts-expect-error
-                if (req.transaction[key]) {
-                    // @ts-expect-error
-                    tx[key] = fn(req.transaction[key]);
-                }
+  async _perform(req: PerformActionRequest): Promise<any> {
+    // Legacy networks do not like the type field being passed along (which
+    // is fair), so we delete type if it is 0 and a non-EIP-1559 network
+    if (req.method === "call" || req.method === "estimateGas") {
+      let tx = req.transaction;
+      if (tx && tx.type != null && getBigInt(tx.type)) {
+        // If there are no EIP-1559 properties, it might be non-EIP-1559
+        if (tx.maxFeePerGas == null && tx.maxPriorityFeePerGas == null) {
+          const feeData = await this.getFeeData();
+          if (feeData.maxFeePerGas == null && feeData.maxPriorityFeePerGas == null) {
+            // Network doesn't know about EIP-1559 (and hence type)
+            req = Object.assign({}, req, {
+              transaction: Object.assign({}, tx, { type: undefined }),
             });
-
-            return { method: "eth_estimateGas", args: [tx] };
+          }
         }
-
-        return super.getRpcRequest(req);
+      }
     }
 
-    async estimateGas(_tx: CeloTransactionRequest): Promise<bigint> {
-        return getBigInt(
-            await this._perform({
-                method: "estimateGas",
-                // @ts-ignore
-                transaction: _tx,
-            }),
-            "%response"
-        );
+    const request = this.getRpcRequest(req);
+
+    if (request != null) {
+      return await this.send(request.method, request.args);
     }
+
+    return super._perform(req);
+  }
+
+  /**
+   * Override to handle alternative gas currencies
+   * prepareRequest in https://github.com/ethers-io/ethers.js/blob/master/packages/providers/src.ts/json-rpc-provider.ts
+   */
+  getRpcRequest(req: PerformActionRequest): null | { method: string; args: Array<any> } {
+    if (req.method === "getGasPrice") {
+      // @ts-expect-error
+      const param = req.feeCurrencyAddress
+        ? // @ts-expect-error
+          [req.feeCurrencyAddress]
+        : [];
+      return { method: "eth_gasPrice", args: param };
+    }
+
+    if (req.method === "estimateGas") {
+      const extraneous_keys = [
+        ["from", (x: string) => x],
+        ["feeCurrency", (x: string) => x],
+      ] as const;
+
+      const tx = {
+        ...this.getRpcTransaction(req.transaction),
+      };
+      extraneous_keys.forEach(([key, fn]) => {
+        // @ts-expect-error
+        if (req.transaction[key]) {
+          // @ts-expect-error
+          tx[key] = fn(req.transaction[key]);
+        }
+      });
+
+      return { method: "eth_estimateGas", args: [tx] };
+    }
+
+    return super.getRpcRequest(req);
+  }
+
+  async estimateGas(_tx: CeloTransactionRequest): Promise<bigint> {
+    return getBigInt(
+      await this._perform({
+        method: "estimateGas",
+        // @ts-ignore
+        transaction: _tx,
+      }),
+      "%response"
+    );
+  }
 
     async getFeeData(feeCurrency?: string): Promise<FeeData> {
         if (!feeCurrency) {
@@ -99,21 +99,21 @@ export default class CeloProvider extends JsonRpcProvider {
         return new FeeData(gasPrice, maxFeePerGas, maxPriorityFeePerGas)
     }
 
-    async broadcastTransaction(signedTx: string): Promise<TransactionResponse> {
-        const { hash } = await resolveProperties({
-            blockNumber: this.getBlockNumber(),
-            hash: this._perform({
-                method: "broadcastTransaction",
-                signedTransaction: signedTx,
-            }),
-            network: this.getNetwork(),
-        });
+  async broadcastTransaction(signedTx: string): Promise<TransactionResponse> {
+    const { hash } = await resolveProperties({
+      blockNumber: this.getBlockNumber(),
+      hash: this._perform({
+        method: "broadcastTransaction",
+        signedTransaction: signedTx,
+      }),
+      network: this.getNetwork(),
+    });
 
-        const tx = parseCeloTransaction(signedTx);
-        if (tx.hash !== hash) {
-            throw new Error("@TODO: the returned hash did not match");
-        }
-
-        return new TransactionResponse(tx as TransactionResponseParams, this);
+    const tx = parseCeloTransaction(signedTx);
+    if (tx.hash !== hash) {
+      throw new Error("@TODO: the returned hash did not match");
     }
+
+    return new TransactionResponse(tx as TransactionResponseParams, this);
+  }
 }
--- tests/common.ts
+++ tests/common.ts
@@ -1,19 +1,16 @@
 import CeloProvider from "../src/lib/CeloProvider";
 import CeloWallet from "../src/lib/CeloWallet";
 import { FORNO_ALFAJORES_URL, CELO_DERIVATION_PATH } from "./consts";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 // Configure dotenv to load test environment variables from `.env.test.local`
-dotenv.config({ path: 'tests/.env.test.local' });
+dotenv.config({ path: "tests/.env.test.local" });
 
 export function getSigner() {
   const provider = new CeloProvider(FORNO_ALFAJORES_URL);
   const mnemonic = process.env.MNEMONIC;
   if (!mnemonic) throw new Error("No MNEMONIC provided in env");
-  const wallet = CeloWallet.fromMnemonic(
-    mnemonic,
-    CELO_DERIVATION_PATH
-  ).connect(provider);
+  const wallet = CeloWallet.fromMnemonic(mnemonic, CELO_DERIVATION_PATH).connect(provider);
   console.log("Using account", wallet.address);
   return wallet;
 }
@@ -29,4 +26,3 @@ export const MINIMAL_USDC_ABI = [
   // Authenticated function
   "function transfer(address to, uint amount) returns (bool)",
 ];
-
